85e4d27dfa878c88878fd9286dcf469b2d2fef38
cl27 <cl27v684i@gmail.com>
2023-07-20 16:10:48 +0700

    Add home.html, requirements.txt

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+
+def testpage(request):
+    return HttpResponse('<h1>this is test page!</h1>')
